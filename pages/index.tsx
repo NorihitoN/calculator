ca46618a8d8a138c0e66c177a23674d2c32d4a72
norihito <norihito0626@gmail.com>
2020-12-07 00:54:11 +0900

    fixed the bug which not reset onClickDigit after onEnterKey

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,19 +27,32 @@ const StyledCode = styled.code`
 const Home = () => {
   const [histories, setHistories] = useState<string[]>([]);
   const [answer, setAnswer] = useState<number>(0);
-  const [display, setDisplay] = useState<string>("0");
+  const [display, setDisplay] = useState<string>("");
   const [currentOperator, setCurrentOperator] = useState<Operator>();
   const [calculated, setCalculated] = useState<boolean>(false);
   const [expression, setExpression] = useState<string>("");
 
   const onDigitKeyClick = (digit: Digit) => {
     console.log(answer, currentOperator, display, calculated);
+
+    // 追加
+    // Enterにて計算したあと、数字を入力すると
+    // answerとexpressionをリセットするため。
+    // onClearKeyClickと共通化可能。
+    if(currentOperator === undefined) {
+      setCalculated(false);
+      setExpression("");
+      setDisplay("0");
+      setAnswer(0);
+    }
+
     let newDisplay = display;
 
     // displayが0のときに0をクリックしても何もしない
     if ((display === "0" && digit === 0) || display.length > 11) {
       return;
     }
+
     // dispalyが0のときに2を押すと2と表示させる。"02"とはならない。
     // OperatorKeyもしくはEnterKeyにて計算されたあと（calculated = trueとなる)に
     // 数値がクリックされると表示はリセットされる。 例）計算結果に14が表示されている状態で、
@@ -72,7 +85,7 @@ const Home = () => {
     }
 
     let newAnswer = answer;
-    if (currentOperator === undefined || currentOperator === "+") {
+    if ( currentOperator === undefined || currentOperator === "+") {
       newAnswer += Number(display);
     } else if (currentOperator === "-") {
       newAnswer -= Number(display);
@@ -86,11 +99,10 @@ const Home = () => {
     setExpression(expression + " " + display + " " + operator )
     setDisplay(newAnswer.toString());
     setCurrentOperator(operator);
-    console.log(answer, currentOperator);
   };
 
   const onEnterKeyClick = () => {
-    console.log(answer, currentOperator, display, calculated);
+    console.log(answer, currentOperator, display, calculated );
     if(currentOperator === undefined) return; 
 
     // Enterを連続で押した場合は無効
@@ -122,7 +134,7 @@ const Home = () => {
   };
 
   const onClearKeyClick = () => {
-    console.log(answer, currentOperator, display, calculated);
+    console.log(answer, currentOperator, display, calculated );
     if(currentOperator === undefined) {
       setCalculated(false);
       setExpression("");
